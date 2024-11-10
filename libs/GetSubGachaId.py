87d7c98851f8ab42260f53605e36653dcb7758de
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-11-10 13:40:39 -0600

    Update GetSubGachaId.py

--- libs/GetSubGachaId.py
+++ libs/GetSubGachaId.py
@@ -4,8 +4,11 @@ import main
 
 from mytime import GetTimeStamp
 
+
 def GetGachaSubIdFP():
-    response = requests.get(f"https://git.atlasacademy.io/atlasacademy/fgo-game-data/raw/branch/JP/master/mstGachaSub.json");
+    response = requests.get(
+        f"https://git.atlasacademy.io/atlasacademy/fgo-game-data/raw/branch/JP/master/mstGachaSub.json"
+    )
     gachaList = json.loads(response.text)
     timeNow = GetTimeStamp()
     priority = 0
@@ -15,21 +18,21 @@ def GetGachaSubIdFP():
         openedAt = gacha["openedAt"]
         closedAt = gacha["closedAt"]
 
-        # 修正逻辑运算符
+        # Fixing logical operators
         if openedAt <= timeNow and timeNow <= closedAt:
             p = int(gacha["priority"])
             if p > priority:
                 priority = p
                 goodGacha = gacha
 
-    # 检查是否找到了合适的 gacha
+    # Verify whether a suitable gacha has been found
     if not goodGacha:
         main.logger.info("No suitable gacha found")
-        return None  
+        return None
 
-    # 确认 'id' 键是否存在
+    # Confirm the 'id' Key exists
     if "id" not in goodGacha:
         main.logger.info("Key 'id' not found in the selected gacha")
-        return None  
+        return None
 
     return str(goodGacha["id"])
