c4abc33a425560d1423c738cb9a636dfcfc53bbd
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-12-04 19:38:18 -0600

    Update CatAndMouseGame.py

--- CatAndMouseGame.py
+++ CatAndMouseGame.py
@@ -7,14 +7,19 @@ import main
 
 def getAssetBundle(assetbundle):
     data = base64.b64decode(assetbundle)
-    key = b"nn33CYId2J1ggv0bYDMbYuZ60m4GZt5P"  # By default is NA
+    key = b'nn33CYId2J1ggv0bYDMbYuZ60m4GZt5P'  # By default is NA
     iv = data[:32]
     array = data[32:]
 
     if main.fate_region == "JP":
-        key = b"W0Juh4cFJSYPkebJB9WpswNF51oa6Gm7"
+        key = b'W0Juh4cFJSYPkebJB9WpswNF51oa6Gm7'
 
-    cipher = py3rijndael.RijndaelCbc(key, iv, py3rijndael.paddings.Pkcs7Padding(16), 32)
+    cipher = py3rijndael.RijndaelCbc(
+        key,
+        iv,
+        py3rijndael.paddings.Pkcs7Padding(16),
+        32
+    )
 
     data = cipher.decrypt(array)
     gzip_data = gzip.decompress(data)
