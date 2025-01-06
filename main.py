73a776ac899052dc4fd7ee18c5b3cd7c451a07d6
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2025-01-06 17:52:29 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,35 +7,37 @@ import user
 import coloredlogs
 import logging
 
-userIds = os.environ['userIds'].split(',')
-authKeys = os.environ['authKeys'].split(',')
-secretKeys = os.environ['secretKeys'].split(',')
-webhook_discord_url = os.environ['webhookDiscord']
-device_info = os.environ.get('DEVICE_INFO_SECRET')
-appCheck = os.environ.get('APP_CHECK_SECRET')
-user_agent_2 = os.environ.get('USER_AGENT_SECRET_2')
-fate_region = 'JP'
+userIds = os.environ["userIds"].split(",")
+authKeys = os.environ["authKeys"].split(",")
+secretKeys = os.environ["secretKeys"].split(",")
+webhook_discord_url = os.environ["webhookDiscord"]
+device_info = os.environ.get("DEVICE_INFO_SECRET")
+appCheck = os.environ.get("APP_CHECK_SECRET")
+user_agent_2 = os.environ.get("USER_AGENT_SECRET_2")
+fate_region = "JP"
 
 userNums = len(userIds)
 authKeyNums = len(authKeys)
 secretKeyNums = len(secretKeys)
 
 logger = logging.getLogger("FGO Daily Login")
-coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
+coloredlogs.install(fmt="%(asctime)s %(name)s %(levelname)s %(message)s")
+
 
 def get_latest_verCode():
-    endpoint = "https://raw.githubusercontent.com/DNNDHH/FGO-VerCode-extractor/JP/VerCode.json"
+    endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/NA/jp.json"
     response = requests.get(endpoint).text
     response_data = json.loads(response)
 
-    return response_data['verCode']
-    
+    return response_data["verCode"]
+
+
 def get_latest_appver():
-    endpoint = "https://raw.githubusercontent.com/DNNDHH/FGO-VerCode-extractor/JP/VerCode.json"
+    endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/NA/jp.json"
     response = requests.get(endpoint).text
     response_data = json.loads(response)
 
-    return response_data['appVer']
+    return response_data["appVer"]
 
 
 def main():
@@ -45,7 +47,7 @@ def main():
             try:
                 instance = user.user(userIds[i], authKeys[i], secretKeys[i])
                 time.sleep(1)
-                logger.info(f"\n {'=' * 40} \n [+] 登录账号 \n {'=' * 40} " )
+                logger.info(f"\n {'=' * 40} \n [+] Login in... \n {'=' * 40} ")
                 instance.topLogin()
                 time.sleep(2)
                 instance.topHome()
@@ -64,10 +66,9 @@ def main():
                 time.sleep(1)
                 instance.drawFP()
 
-
             except Exception as ex:
                 logger.error(ex)
 
+
 if __name__ == "__main__":
     main()
-
