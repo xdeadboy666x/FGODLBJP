c88b8af0bdc8ce84bc4ed97c4f5994fa2261dbd0
David Ramirez <154488832+xdeadboy666x@users.noreply.github.com>
2025-02-25 23:28:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -46,23 +46,22 @@ def main():
         for i in range(userNums):
             try:
                 instance = user.user(userIds[i], authKeys[i], secretKeys[i])
+                time.sleep(3)
+                logger.info(
+                    "\n ======================================== \n [+]Signing in... \n ======================================== "
+                )
+
                 time.sleep(1)
-                logger.info(f"\n {'=' * 40} \n [+] Login in... \n {'=' * 40} ")
-                instance.topLogin()
+                instance.topLogin_s()
                 time.sleep(2)
                 instance.topHome()
-                time.sleep(0.5)
+                time.sleep(2)
                 instance.lq001()
-                time.sleep(0.5)
-                instance.Present()
-                time.sleep(0.5)
                 instance.lq002()
                 time.sleep(2)
                 instance.buyBlueApple()
                 time.sleep(1)
                 instance.lq003()
-                time.sleep(1)
-                instance.LTO_Gacha()
                 time.sleep(1)
                 instance.drawFP()
 
