59add5bc3af12a741c8fe529ee5366ef80e9a8d5
David Ramirez <154488832+xdeadboy666x@users.noreply.github.com>
2025-05-14 00:21:08 -0600

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -111,9 +111,8 @@ def topLogin(data: list) -> None:
     except requests.exceptions.RequestException as e:
         print("Request failed:", e)
 
-
 def shop(item: str, quantity: str) -> None:
-    # def shop(item: str, quantity: int) -> None:
+#def shop(item: str, quantity: int) -> None:
     endpoint = main.webhook_discord_url
 
     jsonData = {
@@ -254,7 +253,7 @@ def LTO_Gacha(servants) -> None:
 
 
 def Present(name, namegift, object_id_count) -> None:
-    # def Present(name: str, namegift: str, object_id_count: int) -> None:
+#def Present(name: str, namegift: str, object_id_count: int) -> None:
     endpoint = main.webhook_discord_url
 
     jsonData = {
