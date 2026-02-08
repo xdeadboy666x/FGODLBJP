7e78269f6c1dfdedce9192a921c0957b2e5dc897
David Ramirez <154488832+xdeadboy666x@users.noreply.github.com>
2026-02-08 09:43:19 +0000

    Add region parameter to GetGachaSubIdFP function

--- libs/GetSubGachaId.py
+++ libs/GetSubGachaId.py
@@ -4,7 +4,7 @@ import main
 
 from mytime import GetTimeStamp
 
-def GetGachaSubIdFP():
+def GetGachaSubIdFP(region):
     response = requests.get(f"https://raw.githubusercontent.com/DNNDHH/GSubList/Main/update.json");
     gachaList = json.loads(response.text)
     url = "https://raw.githubusercontent.com/DNNDHH/GSubList/Main/update.json"
