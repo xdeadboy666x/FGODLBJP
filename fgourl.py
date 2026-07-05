f313c73a8606484a83aa9cbf32a93fe1ba2bfde7
David Ramirez <154488832+xdeadboy666x@users.noreply.github.com>
2026-07-04 22:28:25 -0600

    Clean up fgourl.py by removing unused code
    Removed unused imports and redundant code in set_latest_assets function.

--- fgourl.py
+++ fgourl.py
@@ -26,34 +26,6 @@ def set_latest_assets():
     global app_ver_, data_ver_, date_ver_, asset_bundle_folder_, data_server_folder_crc_, ver_code_, server_addr_
 
     region = main.fate_region
-import json
-import binascii
-import requests
-import main
-import CatAndMouseGame
-import os
-
-requests.urllib3.disable_warnings()
-session = requests.Session()
-session.verify = False
-
-# ===== Game's parameters =====
-app_ver_ = ''
-data_ver_ = 0
-date_ver_ = 0
-ver_code_ = ''
-asset_bundle_folder_ = ''
-data_server_folder_crc_ = 0
-server_addr_ = 'https://game.fate-go.jp'
-github_token_ = ''
-github_name_ = ''
-
-
-# ==== User Info ====
-def set_latest_assets():
-    global app_ver_, data_ver_, date_ver_, asset_bundle_folder_, data_server_folder_crc_, ver_code_, server_addr_
-
-    region = main.fate_region
 
     # Set Game Server Depends of region
 
@@ -112,9 +84,6 @@ def PostReq(s, url, data):
     if res_code != '00':
         detail = res['response'][0]['fail']['detail']
         message = f'[ErrorCode: {res_code}]\n{detail}'
-        raise Exception(message)
-
-    return res        message = f'[ErrorCode: {res_code}]\n{detail}'
         raise Exception(message)
 
     return res
